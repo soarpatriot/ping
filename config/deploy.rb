81cede444c3d50e272baea86109c236cd043299b
Soar <85624529@qq.com>
2016-11-26 00:26:41 +0800

    config the log

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ namespace :deploy do
   task :build do
     on roles(:all), in: :sequence do
       within release_path  do
-        execute :mix, "deps.get && MIX_ENV=#{fetch(:mix_env)} mix compile  && MIX_ENV=#{fetch(:mix_env)} mix release"
+        execute :mix, "deps.get && MIX_ENV=#{fetch(:mix_env)} mix ecto.migrate && MIX_ENV=#{fetch(:mix_env)} mix compile  && MIX_ENV=#{fetch(:mix_env)} mix release"
       end
     end
   end
--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,9 @@ config :ping, Ping.Endpoint,
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, 
+  path: "log/ping.log"
+  level: :info
 
 # ## SSL Support
 #
